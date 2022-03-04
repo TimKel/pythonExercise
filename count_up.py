f33332ad614bbb914cd04d4c317dab72db902819
Tim Kelley <timkelley683@gmail.com>
2022-03-03 18:34:12 -0800

    python exercise

---
+++ count_up.py
@@ -0,0 +1,20 @@
+def count_up(start, stop):
+    """Print all numbers from start up to and including stop.
+
+    For example:
+
+        count_up(5, 7)
+
+   should print:
+
+        5
+        6
+        7
+    """
+    for num in range(5,8):
+        print(num)
+        num = num + 1
+    # YOUR CODE HERE
+
+
+count_up(5, 7)        
